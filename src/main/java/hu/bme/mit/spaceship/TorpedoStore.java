7638bfd5d5a99303df67d37540c039efe2ac0498
unknown <185006@bme.hu>
2025-10-15 15:18:50 +0200

    Added comment to RanGen

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -14,6 +14,7 @@ public class TorpedoStore {
 
   private int torpedoCount = 0;
 
+  //Random Generator to simulate overheating
   private Random generator = new Random();
 
   public TorpedoStore(int numberOfTorpedos){
